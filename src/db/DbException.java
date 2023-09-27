2b621beb7c08875b806536f98ab6e118cb7bf48c
Eduardo Rolim <eduardorolim15@gmail.com>
2023-09-27 08:26:47 -0300

    Creating project and git repository

--- src/db/DbException.java
+++ src/db/DbException.java
@@ -1,11 +1,8 @@
 package db;
 
-import java.sql.Statement;
-
-public class DbException extends RuntimeException{
-
+public class DbException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
-	
+
 	public DbException(String msg) {
 		super(msg);
 	}
